808d40e602069754c6a2f8774a13c70533b16560
Yan Hong <onlyhongyan@gmail.com>
2017-03-25 14:47:03 -0700

    fetch student detail by click

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,36 @@
 var Student = React.createClass({
+    getInitialState: function() {
+        return {}
+    },
+    handleClick: function() {
+        var ReactThis = this;
+        axios.get('http://localhost:3000/student/' + this.props.data._id)
+            .then(function(response) {
+                console.log(response.data);
+                ReactThis.setState(response.data);
+            })
+            .catch(function(error) {
+                console.log(error)
+            })
+    },
     render: function() {
-        return <h2>{this.props.data.firstname}</h2>
+        if (this.state.firstname) {
+            var nameNode = (
+                <div>
+                    <h2>Name: {this.state.firstname}</h2>
+                    <h2>Age: {this.state.age}</h2>
+                    <h2>School: {this.state.school}</h2>
+                </div>
+            )
+        } else {
+            var nameNode = <h2 onClick={this.handleClick}>{this.props.data.firstname}</h2>
+        }
+        return (
+            <div>
+                {nameNode}
+                <hr/>
+            </div>
+        )
     }
 })
 
@@ -22,12 +52,16 @@ var StudentList = React.createClass({
                 console.log(error)
             })
     },
+
+
     render: function() {
+        var ReactThis = this;
         return (<div>
+            <h1>Student List</h1>
             {
                 this.state.studentList.map(
                     function(currentStudent) {
-                        return <Student data={currentStudent}/>
+                        return <Student data={currentStudent} key={currentStudent._id}/>
                     }
                 )
             }
